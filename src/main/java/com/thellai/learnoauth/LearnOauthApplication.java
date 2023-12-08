a75bbb305c70a768c89892d850c9e75ceb22c1f1
Thellai <thellaidhinesh96.2@gmail.com>
2023-12-08 18:24:46 +0530

    first commit : spring security with JWT creating, password encoding & spring oAuth

---
+++ src/main/java/com/thellai/learnoauth/LearnOauthApplication.java
@@ -0,0 +1,15 @@
+package com.thellai.learnoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOauthApplication {
+
+    public static void main( String[] args ) {
+
+        SpringApplication.run(LearnOauthApplication.class, args);
+
+    }
+
+}
